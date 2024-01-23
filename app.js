5631f30e6da4da20b78a0401a521549920c533fe
VeselovTimofey <vestimofey@mail.ru>
2024-01-24 00:03:25 +0300

    feat: logger

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const myError = require('./middlewares/error');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const {
   PATHDB = 'mongodb://127.0.0.1:27017/bitfilmsdb',
@@ -14,8 +15,11 @@ app.use(express.json());
 
 mongoose.connect(PATHDB, {});
 
+app.use(requestLogger);
+
 app.use('/', require('./routes/index'));
 
+app.use(errorLogger);
 app.use(errors());
 app.use(myError);
 
