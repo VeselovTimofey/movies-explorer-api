3547272cbe1db5431da8e5769a6cfe4cd3e7255b
VeselovTimofey <vestimofey@mail.ru>
2024-01-26 00:17:39 +0300

    feat: add helmet and limiter

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const helmet = require('helmet');
 const myError = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const limiter = require('./middlewares/rate_limit');
 const { DEV_PATHDB, PORT } = require('./conf');
 
 const { NODE_ENV, PATHDB } = process.env;
@@ -11,6 +13,8 @@ const { NODE_ENV, PATHDB } = process.env;
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
+app.use(limiter);
 
 mongoose.connect(NODE_ENV === 'production' ? PATHDB : DEV_PATHDB, {});
 
